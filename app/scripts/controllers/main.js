9671578a6f103480dc780725d00701c929f143f3
navneetnagpal <navneetabh@gmail.com>
2014-07-02 10:15:34 +0530

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('uiTestingangularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
